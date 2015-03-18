2cbd9b5d758c9c83308fa2d126f0aa86e12ccbac
Jean Delvare <jdelvare@suse.de>
2015-03-18 14:02:09 +0000

    py-smbus: Linkage fix
    Contributed by Michael Göhler. My original attempt to fix it just
    shows how little I know about python.
    
    
    git-svn-id: http://lm-sensors.org/svn/i2c-tools/trunk@6278 7894878c-1315-0410-8ee3-d5d059ff63e0

--- py-smbus/setup.py
+++ py-smbus/setup.py
@@ -11,4 +11,10 @@ setup(	name="smbus",
 	maintainer_email="linux-i2c@vger.kernel.org",
 	license="GPLv2",
 	url="http://lm-sensors.org/",
-	ext_modules=[Extension("smbus", ["smbusmodule.c"])])
+	ext_modules=[Extension(
+		"smbus",
+		["smbusmodule.c"],
+		extra_compile_args=['-I../include'],
+		extra_link_args=['-L../lib', '-li2c']
+	)]
+)
