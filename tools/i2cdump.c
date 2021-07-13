oudream/i2c-tools

commit 7abc52d7792e0dd6493e32477a2e8675d499c13f

Return the correct error code on I2C block read failure.
git-svn-id: http://lm-sensors.org/svn/i2c-tools/trunk@5073 7894878c-1315-0410-8ee3-d5d059ff63e0

tools/i2cdump.c
@@ -313,8 +313,10 @@ int main(int argc, char *argv[])
 				for (res = 0; res < 256; res += i) {
 					i = i2c_smbus_read_i2c_block_data(file,
 						res, 32, cblock + res);
-					if (i <= 0)
+					if (i <= 0) {
+						res = i;
 						break;
+					}
 				}
 			}
 			if (res <= 0) {

commit 230be0e72f2c8ca2d973218060e044c1dada1254

i2cdump: Remove dead code
Considering that we exit immediately if an error happens during a
block read, the code filling the remaining bytes with -1 (so that
they will be displayed as "XX") is effectively dead code, so let's
remove it.

We might want to revisit the whole logic later, as there's in fact
little reason why block read errors would be fatal when all other
read errors are not. But I'd rather remove SMBus block support from
the tool before attempting to clean up the code.

Fixes: 7abc52d7792e ("Return the correct error code on I2C block read failure.")
Signed-off-by: Jean Delvare <jdelvare@suse.de>
Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

tools/i2cdump.c
@@ -360,9 +360,6 @@ int main(int argc, char *argv[])
 				res = 256;
 			for (i = 0; i < res; i++)
 				block[i] = cblock[i];
-			if (size != I2C_SMBUS_BLOCK_DATA)
-				for (i = res; i < 256; i++)
-					block[i] = -1;
 		}
 
 		if (size == I2C_SMBUS_BYTE) {